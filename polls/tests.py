dbe01faba2166ebff8816c9e44c1fe2a33d6b93b
TirthaPatel978 <ivory32tirthap@gmail.com>
2026-03-26 20:25:29 +0530

    Tutorial 5: Testing

--- polls/tests.py
+++ polls/tests.py
@@ -1,3 +1,71 @@
+import datetime
 from django.test import TestCase
+from django.utils import timezone
+from .models import Question
 
-# Create your tests here.
+def create_question(question_text, days):
+    time = timezone.now() + datetime.timedelta(days=days)
+    return Question.objects.create(question_text=question_text, pub_date=time)
+
+class QuestionModelTests(TestCase):
+
+    def test_was_published_recently_with_future_question(self):
+        time = timezone.now() + datetime.timedelta(days=30)
+        future_question = Question(pub_date=time)
+        
+        self.assertIs(future_question.was_published_recently(), False)
+    
+    def test_was_published_recently_with_old_question(self):
+        time = timezone.now() - datetime.timedelta(days=30)
+        old_question = Question(pub_date=time)
+    
+        self.assertIs(old_question.was_published_recently(), False)
+
+
+    def test_was_published_recently_with_recent_question(self):
+        time = timezone.now() - datetime.timedelta(hours=1)
+        recent_question = Question(pub_date=time)
+    
+        self.assertIs(recent_question.was_published_recently(), True)
+
+class QuestionIndexViewTests(TestCase):
+
+    def test_no_questions(self):
+        response = self.client.get('/polls/')
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, "No polls are available.")
+        self.assertQuerySetEqual(
+            response.context['latest_question_list'],
+            []
+        )
+    def test_past_question(self):
+        create_question("Past question.", days=-30)
+        response = self.client.get('/polls/')
+    
+        self.assertQuerySetEqual(
+            response.context['latest_question_list'],
+            ["Past question."],
+            transform=str
+        )
+
+    def test_future_question(self):
+        create_question("Future question.", days=30)
+        response = self.client.get('/polls/')
+    
+        self.assertContains(response, "No polls are available.")
+        self.assertQuerySetEqual(
+            response.context['latest_question_list'],
+            []
+        )
+
+    def test_past_and_future_questions(self):
+        create_question("Past question.", days=-30)
+        create_question("Future question.", days=30)
+        
+        response = self.client.get('/polls/')
+        
+        self.assertQuerySetEqual(
+            response.context['latest_question_list'],
+            ["Past question."],   
+            transform=str
+        )
