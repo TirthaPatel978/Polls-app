4b7e10de880b09a4f56d25ab64436595eaf3b87f
TirthaPatel978 <ivory32tirthap@gmail.com>
2026-03-28 20:57:54 +0530

    Tutorial 7(Customizing Admin form)

--- mysite/settings.py
+++ mysite/settings.py
@@ -54,6 +54,7 @@ ROOT_URLCONF = 'mysite.urls'
 
 TEMPLATES = [
     {
+        
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
